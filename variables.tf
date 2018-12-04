cf95e7e4ee166fc1e1b865ba6bcf7ac774d28859
stoffee <chris@roadkill.org>
2018-12-03 19:46:47 -0800

    sha sha sha shazam

--- variables.tf
+++ variables.tf
@@ -7,6 +7,10 @@ variable "gcp_project_id" {
   description = "The name of the GCP Project where all resources will be launched."
 }
 
+variable "gcp_credentials" {
+  description = "The name of the GCP Project where all resources will be launched."
+}
+
 variable "gcp_region" {
   description = "The region in which all GCP resources will be launched."
 }
